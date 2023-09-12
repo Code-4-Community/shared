08f681ec4eb1d7429052fd16e5469cdbb0f0f02d
Kenny Jung <kenny.jung@rapyuta-robotics.com>
2023-09-12 18:52:04 +0900

    Initial commit

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import type { ButtonHTMLAttributes } from 'react'
+
+import type { Meta, Story } from '@storybook/react'
+
+import { Button as ButtonComponent } from './Button'
+
+export default {
+  title: 'Button',
+} as Meta<ButtonHTMLAttributes<HTMLButtonElement>>
+
+const Template: Story<ButtonHTMLAttributes<HTMLButtonElement>> = (args) => (
+  <ButtonComponent {...args}>Changed button</ButtonComponent>
+)
+
+export const Button = Template.bind({})
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import type { ButtonHTMLAttributes } from 'react'
+
+export function Button({
+  children,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button type="button" {...props}>
+      {children}
+    </button>
+  )
+}
